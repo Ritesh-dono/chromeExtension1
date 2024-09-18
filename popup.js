a0c4450e360a13c0742c39895b13f77fcd3a8e28
Ritesh-dono <riteshkanwasi60@gmail.com>
2024-09-18 16:40:03 +0530

    first-commit

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.getElementById('getTitleBtn').addEventListener('click', () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const currentTab = tabs[0];
+      const title = currentTab.title;
+      document.getElementById('title').textContent = `Tab Title: ${title}`;
+    });
+  });
+  
